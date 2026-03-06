dfa1676cab1598f55535dac6c6b236f11343af30
JFTEC36 <jftecnologia36@gmail.com>
2026-03-06 08:37:34 -0300

    Update system-requirements.sh

--- system-requirements.sh
+++ system-requirements.sh
@@ -45,7 +45,8 @@ sudo apt remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-
 sudo apt install -y ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
